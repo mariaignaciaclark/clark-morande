21bc4a65093a8167f8c1761c188e78fdd685903b
mariaignaciaclark <48286052+mariaignaciaclark@users.noreply.github.com>
2019-04-14 20:06:17 -0400

    Add files via upload

---
+++ portafolio.js
@@ -0,0 +1,43 @@
+// PRIMERO CREAMOS UNA VARIABLE QUE CONTIENE TODOS LOS DATOS DE TUS TRABAJOS
+var trabajos=[
+{
+    figure: "media/foto-01.jpg",
+    title: "Individuales",
+    about: "Decorando tus mesas de verano",
+}, {
+    figure: "media/foto-02.jpg",
+    title: "Manteles",
+    about: "Hechos a medida especialmente para tu casa",
+}, {
+    figure: "media/foto-03.jpg",
+    title: "Servilletas",
+    about: "Útiles & versátiles para todo",
+}, {
+    figure: "media/foto-04.jpg",
+    title: "Cojines",
+    about: "Decorando tus rincones",
+}, {
+    figure: "media/foto-05.jpg",
+    title: "Pareos",
+    about: "Ideales para tu look playero",
+}, {
+    figure: "media/foto-06.jpg",
+    title: "Delantales",
+    about: "Protegiendo tu ropa con estilo",
+}, {
+    figure: "media/foto-07.jpg",
+    title: "Regalos",
+    about: "Regala y sorprende",
+}, {
+    figure: "media/foto-07.jpg",
+    title: "Regalos",
+    about: "Regala y sorprende",
+}
+]
+// LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
+$.each(trabajos, function(i, val) {
+    var uno = '<figure><img src="' + trabajos[i].figure + '"/></figure>';
+    var dos = '<h4>' + trabajos[i].title + '</h4>';
+    var tres = '<p>' + trabajos[i].about + '</p>';
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+});
